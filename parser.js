dbed55897a346a03ddfecbe05948eccc9ab75876
IVAN <na-613c>
2019-06-21 14:38:49 +0300

    refactoring version 1

--- parser.js
+++ parser.js
@@ -1,53 +1,41 @@
-let fetch = require("node-fetch");
-let cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const startUrl = "https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page=";
+const url = startUrl + "0";
+const obj = { arrayOfWorkers: [] };
 
-const obj = {
-  arr: []
-};
-
-
-let startUrl = "https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page=";
-
-  let url = startUrl + "0";
-  parserPage(url);
-
-
+parserPage(url);
 
 function parserPage(url) {
-  fetch(url)
-    .then(function (response) {
-      if (response.status !== 200) {
-        return Promise.reject(new Error(response.statusText));
-      }
-      return Promise.resolve(response);
-    })
-    .then(function (response) {
-      return response.text();
-    })
 
+  function err(response) {
+    if (response.status !== 200) {
+      return Promise.reject(new Error(response.statusText));
+    }
+    return Promise.resolve(response);
+  }
+
+  function text(response) {
+    return response.text();
+  }
+
+  function catchErr(error) {
+    console.log("error on " + url + " : " + error);
+  }
+
+  fetch(url)
+    .then(err)
+    .then(text)
     .then(function (body) {
       let $ = cheerio.load(body);
 
       $(".resume-search-item__header").each((i, el) => {
         let newUrl = $(el).find("a").attr("href");
-        
-        
+
         fetch("https://jobs.tut.by" + newUrl)
-
-          .then(function (response) {
-            if (response.status !== 200) {
-              return Promise.reject(new Error(response.statusText));
-            }
-            return Promise.resolve(response);
-          })
-
-          .then(function (response) {
-            return response.text();
-
-          })
-
+          .then(err)
+          .then(text)
           .then(function (body) {
-
             let $ = cheerio.load(body),
               job = $("h1.header").text(),
               pay = $("span[data-qa = 'resume-block-salary']").text(),
@@ -55,36 +43,42 @@ function parserPage(url) {
               gender = $("span[data-qa = 'resume-personal-gender']").text(),
               age = $("span[data-qa ='resume-personal-age']").text(),
               location = $("span[itemprop  = 'addressLocality'][data-qa ='resume-personal-address']").text(),
-              about = $(".resume-block-container").text();
+              emptyLine = "не указанно";
 
-              
-            let jobInJson = {job,pay,hobby,gender,age,location};
+            switch ("") {
+              case (job): job = emptyLine;
+                break;
+              case (pay): pay = emptyLine;
+                break;
+              case (hobby): hobby = emptyLine;
+                break;
+              case (gender): gender = emptyLine;
+                break;
+              case (age): age = emptyLine;
+                break;
+              case (location): location = emptyLine;
+                break;
+            }
 
-            obj.arr.push({ jobInJson });
+            obj.arrayOfWorkers.push({ job, pay, hobby, gender, age, location });
 
-            let vacancy = JSON.stringify(obj);
+            let vacancy = JSON.stringify(obj.arrayOfWorkers);
             writeInFile(vacancy);
           })
-          .catch(function (error) {
-            console.log("job reference error: " + error);
-          });
+          .catch(catchErr);
       })
 
       $("a[data-qa='pager-next']").each((i, el) => {
         nextPageUrl = "https://jobs.tut.by" + $("a.bloko-button.HH-Pager-Controls-Next.HH-Pager-Control[data-qa='pager-next']").attr("href");
-        console.log("Переход по ссылке "+ nextPageUrl);
+        console.log("Переход по ссылке " + nextPageUrl);
         parserPage(nextPageUrl);
-    });
-
+      });
     })
-
-    .catch(function (error) {
-      console.log("error on " + url + " : " + error);
-    });
+    .catch(catchErr);
 }
 
 function writeInFile(vacancy) {
-  var fs = require('fs')
+  const fs = require('fs')
   fs.writeFileSync('jobs.json', vacancy);
 }
 
