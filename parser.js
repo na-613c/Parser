deb891747f128c3348c5f521f8ceb55a746a66ff
IVAN <na-613c>
2019-06-25 23:50:20 +0300

    no use cherio

--- parser.js
+++ parser.js
@@ -1,86 +1,125 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
-const generalUrl = "https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page=";
-const parsingStartPage = "248";
-const startUrl = generalUrl + parsingStartPage;
-const obj = { arrayOfWorkers: [] };
+var tress = require('tress');
+var needle = require('needle');
+var fs = require('fs');
+var DomParser = require('dom-parser');
+var parser = new DomParser();
 
-parserPage(startUrl, err, text, catchErr);
+let startPageForParsing = 249;
+var URL = 'https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page='+startPageForParsing;
+var results = [];
 
-function err(response) {
-  if (response.status !== 200) {
-    return Promise.reject(new Error(response.statusText));
-  }
-  return Promise.resolve(response);
-}
 
-function text(response) {
-  return response.text();
-}
+let q = tress(function (url, callback) {
+    needle.get(url, function (err, res) {
+        if (err) throw err;
 
-function catchErr(error) {
-  console.log("error on " + startUrl + " : " + error);
-}
+        // парсим DOM
+        const doc = parser.parseFromString(res.body); 
+        console.log("URL = " + URL);
+
+        resumeOnMainPage(doc);
+        movingOnPage(doc);
+
+
+        //информация о резюме
+        function resumeOnMainPage(doc){
+            const resumeUrl = doc.getElementsByClassName('resume-search-item__name'); 
+            const resumeArray = Array.from(resumeUrl); 
+            resumeArray.forEach((resumeUrl) => { 
+            let newResumeUrl = resumeUrl.getAttribute('href'); 
+                parserPage.push("https://jobs.tut.by" + newResumeUrl); 
+            })
+        }
+
+
+        //переход
+        function movingOnPage(doc){
+            const nextPage = doc.getElementsByClassName('bloko-button.HH-Pager-Controls-Next'); 
+            const resumeArray = Array.from(nextPage); 
+            resumeArray.forEach((nextPage) => { 
+            let nextPageUrl = nextPage.getAttribute('href'); 
+            URL = ("https://jobs.tut.by" + nextPageUrl).trim();
+            console.log(URL);
+            q.push(URL);
+            })
+        }
+        callback();
+    });
+}, 10); // запускаем 10 параллельных потоков
+
+let parserPage = tress(function (url, callback) {
+    needle.get(url, function (err, res) {
+        if (err) throw err;
+
+        // парсим DOM
+        const doc = parser.parseFromString(res.body); 
+        // JOB
+            let jobElement = doc.getElementsByClassName('resume-block__title-text');
+            const jobArray = [];
+            let jobCollection = Array.from(jobElement);
+            jobCollection.forEach((jobElement) => {jobArray.push(jobElement.innerHTML);})
+            let job = jobArray[0];
+        //   PAY
+            let payElement = doc.getElementsByClassName('resume-block__title-text_salary');
+            const payArray = [];
+            let payCollection = Array.from(payElement);
+            payCollection.forEach((payElement) => {payArray.push(payElement.innerHTML);})
+            let pay = payArray[0];
+        // GENDER
+        let aboutElement = doc.getElementsByClassName('resume-header-block');
+        const aboutArray = [];
+        let aboutCollection = Array.from(aboutElement);
+        aboutCollection.forEach((aboutElement) => {
+            let regExp = /<!-- -->|(|)|/g;
+            
+            aboutArray.push(aboutElement.textContent.replace(regExp,""));
+        })
+        
+        let about = aboutArray[0];
+        about = about.split(', ');
+        let genderAndAge = about.splice(0, 2);
+        
+        let local = about.splice(0, 1);
+        local = local.join(',');
+        number = local.lastIndexOf(1||2);
+
+        
+        let location = local.slice(number+4) + ", " + about;
+            console.log("Cтрока = "+location+' ___||||___ ' + local+" ");
+
+        let gender = genderAndAge[0],
+            age = genderAndAge[1],
+            
+            emptyLine = "не указанно";
+
+            
+
+
+
+        switch (undefined) {
+            case (job): job = emptyLine;
+                break;
+            case (pay): pay = emptyLine;
+                break;
+            case (gender): gender = emptyLine;
+                break;
+            case (age): age = emptyLine;
+                break;
+            case (location): location = emptyLine;
+                break;
+        }
+
+        results.push({ job, pay, gender, age, location, url });
+
+        let vacancy = JSON.stringify(results);
+        writeInFile(vacancy);
+
+        callback();
+    });
+}, 20); // запускаем 20 параллельных потоков
 
 function writeInFile(vacancy) {
-  const fs = require('fs');
-  fs.writeFileSync('jobs.json', vacancy);
+    fs.writeFileSync('jobs.json', vacancy);
 }
 
-function parserPage(url, err, text, catchErr) {
-  fetch(url)
-    .then(err)
-    .then(text)
-    .then(function (body) {
-      let $ = cheerio.load(body);
-
-      $(".resume-search-item__header").each((i, el) => {
-        let newUrl = $(el).find("a").attr("href");
-        let url = "https://jobs.tut.by" + newUrl;
-
-        fetch(url)
-          .then(err)
-          .then(text)
-          .then(function (body) {
-            let $ = cheerio.load(body),
-              job = $("h1.header").text(),
-              pay = $("span[data-qa = 'resume-block-salary']").text(),
-              // about = $("span[data-qa = 'resume-block-specialization-category']").text(),
-              about = $("div.resume-block-container[data-qa='resume-block-skills-content']").text().replace(/||\r?\n/g, ""),
-              gender = $("span[data-qa = 'resume-personal-gender']").text(),
-              age = $("span[data-qa ='resume-personal-age']").text(),
-              location = $("span[itemprop  = 'addressLocality'][data-qa ='resume-personal-address']").text(),
-              emptyLine = "не указанно";
-
-            switch ("") {
-              case (job): job = emptyLine;
-                break;
-              case (pay): pay = emptyLine;
-                break;
-              case (about): about = emptyLine;
-                break;
-              case (gender): gender = emptyLine;
-                break;
-              case (age): age = emptyLine;
-                break;
-              case (location): location = emptyLine;
-                break;
-            }
-
-            obj.arrayOfWorkers.push({ job, pay, gender, age, location,about , url });
-
-            let vacancy = JSON.stringify(obj);
-            writeInFile(vacancy);
-          })
-          .catch(catchErr);
-      })
-
-      $("a[data-qa='pager-next']").each((i, el) => {
-        nextPageUrl = "https://jobs.tut.by" + $("a.bloko-button.HH-Pager-Controls-Next.HH-Pager-Control[data-qa='pager-next']").attr("href");
-        console.log("Переход по ссылке " + nextPageUrl);
-        parserPage(nextPageUrl, err, text, catchErr);
-      });
-    })
-    .catch(catchErr);
-}
-
+q.push(URL);
