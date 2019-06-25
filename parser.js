2dcd56d39aa5bc732029b0d98c8c9fc08c953fc3
IVAN <na-613c>
2019-06-26 00:10:51 +0300

    location works correctly

--- parser.js
+++ parser.js
@@ -5,16 +5,16 @@ var DomParser = require('dom-parser');
 var parser = new DomParser();
 
 let startPageForParsing = 249;
-var URL = 'https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page='+startPageForParsing;
+var URL = 'https://jobs.tut.by/search/resume?L_is_autosearch=false&area=1002&clusters=true&currency_code=BYR&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&text=&page=' + startPageForParsing;
 var results = [];
 
 
-let q = tress(function (url, callback) {
+let mainPage = tress(function (url, callback) {
     needle.get(url, function (err, res) {
         if (err) throw err;
 
         // парсим DOM
-        const doc = parser.parseFromString(res.body); 
+        const doc = parser.parseFromString(res.body);
         console.log("URL = " + URL);
 
         resumeOnMainPage(doc);
@@ -22,25 +22,25 @@ let q = tress(function (url, callback) {
 
 
         //информация о резюме
-        function resumeOnMainPage(doc){
-            const resumeUrl = doc.getElementsByClassName('resume-search-item__name'); 
-            const resumeArray = Array.from(resumeUrl); 
-            resumeArray.forEach((resumeUrl) => { 
-            let newResumeUrl = resumeUrl.getAttribute('href'); 
-                parserPage.push("https://jobs.tut.by" + newResumeUrl); 
+        function resumeOnMainPage(doc) {
+            const resumeUrl = doc.getElementsByClassName('resume-search-item__name');
+            const resumeArray = Array.from(resumeUrl);
+            resumeArray.forEach((resumeUrl) => {
+                let newResumeUrl = resumeUrl.getAttribute('href');
+                parserPage.push("https://jobs.tut.by" + newResumeUrl);
             })
         }
 
 
         //переход
-        function movingOnPage(doc){
-            const nextPage = doc.getElementsByClassName('bloko-button.HH-Pager-Controls-Next'); 
-            const resumeArray = Array.from(nextPage); 
-            resumeArray.forEach((nextPage) => { 
-            let nextPageUrl = nextPage.getAttribute('href'); 
-            URL = ("https://jobs.tut.by" + nextPageUrl).trim();
-            console.log(URL);
-            q.push(URL);
+        function movingOnPage(doc) {
+            const nextPage = doc.getElementsByClassName('bloko-button.HH-Pager-Controls-Next');
+            const resumeArray = Array.from(nextPage);
+            resumeArray.forEach((nextPage) => {
+                let nextPageUrl = nextPage.getAttribute('href');
+                URL = ("https://jobs.tut.by" + nextPageUrl).trim();
+                console.log(URL);
+                mainPage.push(URL);
             })
         }
         callback();
@@ -52,49 +52,64 @@ let parserPage = tress(function (url, callback) {
         if (err) throw err;
 
         // парсим DOM
-        const doc = parser.parseFromString(res.body); 
+        const doc = parser.parseFromString(res.body);
         // JOB
-            let jobElement = doc.getElementsByClassName('resume-block__title-text');
-            const jobArray = [];
-            let jobCollection = Array.from(jobElement);
-            jobCollection.forEach((jobElement) => {jobArray.push(jobElement.innerHTML);})
-            let job = jobArray[0];
+        let jobElement = doc.getElementsByClassName('resume-block__title-text');
+        const jobArray = [];
+        let jobCollection = Array.from(jobElement);
+        jobCollection.forEach((jobElement) => { jobArray.push(jobElement.innerHTML); })
+        let job = jobArray[0];
         //   PAY
-            let payElement = doc.getElementsByClassName('resume-block__title-text_salary');
-            const payArray = [];
-            let payCollection = Array.from(payElement);
-            payCollection.forEach((payElement) => {payArray.push(payElement.innerHTML);})
-            let pay = payArray[0];
+        let payElement = doc.getElementsByClassName('resume-block__title-text_salary');
+        const payArray = [];
+        let payCollection = Array.from(payElement);
+        payCollection.forEach((payElement) => { payArray.push(payElement.innerHTML); })
+        let pay = payArray[0];
         // GENDER
         let aboutElement = doc.getElementsByClassName('resume-header-block');
         const aboutArray = [];
         let aboutCollection = Array.from(aboutElement);
         aboutCollection.forEach((aboutElement) => {
             let regExp = /<!-- -->|(|)|/g;
-            
-            aboutArray.push(aboutElement.textContent.replace(regExp,""));
+
+            aboutArray.push(aboutElement.textContent.replace(regExp, ""));
         })
-        
+
         let about = aboutArray[0];
         about = about.split(', ');
         let genderAndAge = about.splice(0, 2);
-        
+
         let local = about.splice(0, 1);
         local = local.join(',');
-        number = local.lastIndexOf(1||2);
+        let counter = 0;
+        let maxIndex = 0;
 
-        
-        let location = local.slice(number+4) + ", " + about;
-            console.log("Cтрока = "+location+' ___||||___ ' + local+" ");
+        searchIdCity(local, counter, maxIndex);
+
+        function searchIdCity(local, counter) {
+            let number = local.lastIndexOf(counter);
+            if (maxIndex < number) {
+                maxIndex = number;
+            }
+            counter++;
+            if (counter < 10) {
+                searchIdCity(local, counter, maxIndex);
+            }
+        }
+
+
+        if (maxIndex != 0) {
+            maxIndex++;
+        }
+
+
+        let location = local.slice(maxIndex) + ", " + about;
+        console.log("Cтрока = " + location + ' ___||||___ ' + local + " ");
 
         let gender = genderAndAge[0],
             age = genderAndAge[1],
-            
+
             emptyLine = "не указанно";
-
-            
-
-
 
         switch (undefined) {
             case (job): job = emptyLine;
@@ -122,4 +137,4 @@ function writeInFile(vacancy) {
     fs.writeFileSync('jobs.json', vacancy);
 }
 
-q.push(URL);
+mainPage.push(URL);
